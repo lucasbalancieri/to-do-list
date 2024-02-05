9e49e4163e04925cf4fed63c68834c3b5ccd7e2d
Lucas Balancieri <lucas.balancieri@outlook.com>
2024-02-05 16:04:48 -0300

    adiciona função para remover tarefa da lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,40 @@ let counter = 1;
 function adicionar() {
     const tarefaInpt = document.querySelector('input#insere-tarefa');
     const tarefaList = document.querySelector('ul#lista-tarefa');
-    
+
     (tarefaInpt.value.length !== 0) ? adicionarTarefa(tarefaInpt, tarefaList) : window.alert('tarefa vazia');
 
 }
 
-// gera id para os itens da lista
+// gera id para os itsens da lista
 function gerarId() {
     return counter++
 }
 
 // cria tarefa para ser incluida na lista de tarefas;
 function criarTarefa(tarefa) {
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     li.id = gerarId();
-    li.innerHTML = `<input type="checkbox" class='checkbox'><span> ${tarefa}</span>`
+    li.innerHTML = `<input type="checkbox" class='checkbox'>
+                    <span> ${tarefa}</span>
+                    <input type="button" value="-" onclick="removerTarefa(${li.id})">`
     return li;
 }
 
-function adicionarTarefa(tarefaInpt, tarefaList){
-    let novaTarefa = criarTarefa(tarefaInpt.value);
+// adiciona tarefa na lista
+function adicionarTarefa(tarefaInpt, tarefaList) {
+    const novaTarefa = criarTarefa(tarefaInpt.value);
     tarefaList.appendChild(novaTarefa); // adiciona a tarefa na lista de tarefas.
     tarefaInpt.value = ''; // limpa o input
+}
+
+//remove tarefa da lista
+function removerTarefa(id) {
+    const tarefaList = document.querySelector('ul#lista-tarefa');
+    const rmvTarefa = document.getElementById(id)
+
+    if (rmvTarefa) { // valida se o item a ser removido está na lista
+        tarefaList.removeChild(rmvTarefa);
+        return window.alert('tarefa removida');
+    };
 }
